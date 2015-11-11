6061af7751009c1772b4e77bea53317f336f86b6
Mrfuzzy <league@iMac.attlocal.net>
2015-11-10 18:35:17 -0800

    I did pinthetailonthedonkey (processing) and finished crazycatlady (eclipse)

--- src/CrazyCatLady.java
+++ src/CrazyCatLady.java
@@ -5,15 +5,17 @@ import javax.swing.JOptionPane;
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-String catty = JOptionPane.showInputDialog("How many cats do you have");
+		int catty = JOptionPane.showOptionDialog(null,
+				"How many cats do you have?", "Catty or no?", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "0",
+						"1", "2", "More than 2" }, null);
 		// 2. Convert their answer into an int
-int cats = Integer.parseInt(catty);
 		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-if(cats > 3){
+if(catty == 3){
 	JOptionPane.showMessageDialog(null, "You are a crazy cat lady, dude.");
 }
 		// 4. If they have 3 or less, call the method below to show them a cat video
-else{
+if(catty == 2 || catty == 0 || catty == 1){
 	playVideo("https://www.youtube.com/watch?v=8HVWitAW-Qg");
 }
 		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
